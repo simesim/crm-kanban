4d3460adb024d65ca1fb999b3b5566641927d30a
Sima <serafiakrav@gmail.com>
2026-01-17 11:35:03 +0500

    DB connection

--- app/schemas/board.py
+++ app/schemas/board.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 
 class BoardCreate(BaseModel):
-    title: str = Field(min_length=1, max_length=200)
+    title: str = Field(min_length = 1, max_length=200)
 
 
 class BoardOut(BaseModel):
@@ -11,3 +11,4 @@ class BoardOut(BaseModel):
     title: str
     owner_id: int
     created_at: datetime
+""" ыфыф """
